a6f6d54d8e0bfcce66219af2d1f2966724cdbb0a
Mickeydoe <michaeldanquah615@gmail.com>
2024-05-15 15:37:27 +0000

    Public access configuration

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 #create s3 bucket
-
 resource "aws_s3_bucket" "mybucket" {
   bucket = var.bucketname
 
@@ -7,4 +6,31 @@ resource "aws_s3_bucket" "mybucket" {
     Name        = "Portfolio bucket"
     
   }
+}
+
+resource "aws_s3_bucket_ownership_controls" "bucker_ownership" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket_access" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "acl_bucket" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.mybucket.id
+  acl    = "public-read"
 }
